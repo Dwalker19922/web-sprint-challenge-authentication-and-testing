20e64ca5245cf1fae531107bf60dca93e9e5b5d5
Daniel Walker <dw765608@gmail.com>
2021-06-18 16:30:02 -0400

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -16,3 +16,17 @@ afterAll(async () => {
 test('sanity', () => {
   expect(true).toBe(true)
 })
+describe('register user', () => {
+  it('registers a new user to the db', async () => {
+    const res = await request(server).post('/api/auth/register')
+    .send({username:"Daniel",password:"1234"})
+    expect(res.status).toBe(201)
+
+  })
+  it('returns data in the correct shape', async () => {
+    const res = await request(server).post('/api/auth/register')
+    .send({username:"Daniel",password:"1234"})
+    expect(res.body).toMatchObject({ id: 1,username:"Daniel" })
+    expect(res.body.password).toBeDefined()
+  })
+})
